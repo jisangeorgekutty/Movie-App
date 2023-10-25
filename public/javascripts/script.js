d7e7e3f80b15484c380ca587f83980e0776eec8a
jisangeorgekutty <jisangeorge123@gmail.com>
2023-10-25 20:28:41 +0530

    first commit

---
+++ public/javascripts/script.js
@@ -0,0 +1,7 @@
+// const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&page=1';
+// const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+// const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=41ee980e4b5f05f6693fda00eb7c4fd4&query=";
+
+// var section=document.getElementById('section');
+// var form=document.getElementById('form');
+// var search=document.getElementById('')
